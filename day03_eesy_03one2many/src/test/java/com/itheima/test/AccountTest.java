6f3b4ffd3aa5fc60d978b49ad71abd8f70c43933
SJHwei <2863254966@qq.com>
2021-03-08 22:59:49 +0800

    Mybatis中多表查询之完成account一对一操作的方法一--建立实体类关系的方式

---
+++ day03_eesy_03one2many/src/test/java/com/itheima/test/AccountTest.java
@@ -0,0 +1,74 @@
+package com.itheima.test;
+
+import com.itheima.dao.IAccountDao;
+import com.itheima.dao.IUserDao;
+import com.itheima.domain.Account;
+import com.itheima.domain.AccountUser;
+import com.itheima.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author ShiWei
+ * @date 2021/3/8 - 22:31
+ */
+public class AccountTest {
+
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IAccountDao accountDao;
+
+    @Before//用于在测试方法执行之前执行
+    public void init()throws Exception{
+        //1.读取配置文件，生成字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取SqlSessionFactory
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession();
+        //4.获取dao的代理对象
+        accountDao = sqlSession.getMapper(IAccountDao.class);
+    }
+
+    @After//用于在测试方法执行之后执行
+    public void destroy()throws Exception{
+        //提交事务
+        sqlSession.commit();
+        //6.释放资源
+        sqlSession.close();
+        in.close();
+    }
+
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public void testFindAll(){
+        //5.执行查询所有方法
+        List<Account> accounts = accountDao.findAll();
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+    }
+
+    /**
+     * 测试查询所有账户，同时包含用户名称和地址
+     */
+    @Test
+    public void testFindAllAccountUser(){
+        //5.执行查询所有方法
+        List<AccountUser> aus = accountDao.findAllAccount();
+        for (AccountUser au : aus) {
+            System.out.println(au);
+        }
+    }
+}
