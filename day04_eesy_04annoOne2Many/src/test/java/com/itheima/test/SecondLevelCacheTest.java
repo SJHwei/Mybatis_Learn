1f8dfc02a857c48efc78903d0add1025d60c5728
SJHwei <2863254966@qq.com>
2021-03-14 23:06:32 +0800

    Mybatis注解开发缓存--一级缓存本来就有，不用考虑，多注意二级缓存

--- day04_eesy_04annoOne2Many/src/test/java/com/itheima/test/SecondLevelCacheTest.java
+++ day04_eesy_04annoOne2Many/src/test/java/com/itheima/test/SecondLevelCacheTest.java
@@ -3,7 +3,6 @@ package com.itheima.test;
 import com.itheima.dao.IUserDao;
 import com.itheima.domain.User;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.jdbc.SQL;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -12,14 +11,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
-import java.util.Date;
-import java.util.List;
 
 /**
  * @author ShiWei
- * @date 2021/3/14 - 20:24
+ * @date 2021/3/14 - 22:52
  */
-public class AnnotationCRUDTest {
+public class SecondLevelCacheTest {
+
     private InputStream in;
     private SqlSessionFactory factory;
     private SqlSession session;
@@ -35,37 +33,26 @@ public class AnnotationCRUDTest {
 
     @After
     public void destroy() throws Exception {
-        session.commit();
-        session.close();
         in.close();
-    }
-
-    @Test
-    public void testFindAll() {
-        List<User> users = userDao.findAll();
-        for (User user : users) {
-            System.out.println(user);
-        }
     }
 
     @Test
     public void testFindOne() {
 
-        User user1 = userDao.findById(61);
+        User user = userDao.findById(61);
+        System.out.println(user);
+        session.close(); //释放一级缓存
+
+        SqlSession session1 = factory.openSession(); //再次打开session
+        IUserDao userDao1 = session1.getMapper(IUserDao.class);
+
+        User user1 = userDao1.findById(61);
         System.out.println(user1);
 
-        User user2 = userDao.findById(61);
-        System.out.println(user2);
+        session1.commit();
+        session1.close();
 
         //测试一级缓存，不用过多考虑一级缓存，一级缓存本来就有。
-        System.out.println(user1 == user2);
-    }
-
-    @Test
-    public void testFindByName() {
-        List<User> users = userDao.findUserByName("%mybatis%");
-        for (User user : users) {
-            System.out.println(user);
-        }
+        System.out.println(user == user1);
     }
 }
