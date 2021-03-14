91e9525e6e84766e331b5061e8fd0888dda1d2a2
SJHwei <2863254966@qq.com>
2021-03-14 22:29:47 +0800

    Mybatis注解开发一对一（多对一）的查询配置

---
+++ day04_eesy_04annoOne2Many/src/main/java/com/itheima/dao/IAccountDao.java
@@ -0,0 +1,30 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Account;
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.FetchType;
+
+import java.util.List;
+
+/**
+ * @author ShiWei
+ * @date 2021/3/14 - 22:03
+ */
+public interface IAccountDao {
+
+    /**
+     * 查询所有账户，并且获取每个账户所属的用户信息
+     * @return
+     */
+    @Select("select * from account")
+    @Results(id = "accountMap", value = {
+            @Result(id=true,column = "id",property = "id"),
+            @Result(column = "uid",property = "uid"),
+            @Result(column = "money",property = "money"),
+            @Result(property = "user", column = "uid",one = @One(select = "com.itheima.dao.IUserDao.findById",fetchType = FetchType.EAGER)),
+    })
+    List<Account> findAll();
+}
